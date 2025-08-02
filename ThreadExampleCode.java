eeefb359b241d74ce7b3e3a5c23004033d23bcc1
ShahzadArshad786 <shahzad.arshad1001@gmail.com>
2025-08-02 14:26:06 +0500

    first commit

---
+++ ThreadExampleCode.java
@@ -0,0 +1,39 @@
+class Demo extends Thread
+{
+    
+    @Override
+    public void run()
+    {
+        try 
+        {
+            for(int i = 1 ; i <= 5 ; i++)
+            {
+                System.out.println("Child Thread");
+                Thread.sleep(1000);
+            }
+        }
+        
+        catch(InterruptedException i )
+        {
+
+        }
+    }
+}
+
+public class ThreadExampleCode {
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        
+        Demo obj = new Demo();
+
+        obj.start();
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.println("Main Thread");
+            Thread.sleep(1000);
+        }
+    }
+    
+}
---
+++ p1.py
@@ -0,0 +1,15 @@
+
+s = 1 
+
+count = 0 
+
+while (s <= 20):
+
+    if((s % 2) == 0):
+        print(s , "is Even Number")
+    else:
+        count = count + 1
+    s = s + 1
+
+print("Total Odd Number : " , count)
+
