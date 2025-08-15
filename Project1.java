53983c52e856ac58e0f326513715bcc56f3041d2
ShahzadArshad786 <shahzad.arshad1001@gmail.com>
2025-08-15 15:38:35 +0500

    "first commit"

--- Project1.java
+++ Project1.java
@@ -1,6 +1,35 @@
+
+
+class Demo extends Thread
+{
+    public void run()
+    {
+        try
+        {
+            for (int i = 1 ; i <=5 ; i++)
+            {
+                System.out.println("Child Thread");
+                Thread.sleep(1000);
+            }
+        }
+        catch(InterruptedException ex)
+        {
+
+        }
+    }
+}
+
 public class Project1 {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello, Welcome to Java Class");
+    public static void main(String[] args) throws InterruptedException
+    
+    {
+        Demo obj = new Demo();
+        obj.start();
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.println("Main Thread");
+            Thread.sleep(1000);
+        }
     }
 }
